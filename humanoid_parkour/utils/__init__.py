5a1f3e367a2732059b27a023a9184be050547ec8
Maoruolin <1539950660@qq.com>
2026-06-01 14:10:05 +0800

    Clarify repo structure

--- humanoid_parkour/utils/__init__.py
+++ humanoid_parkour/utils/__init__.py
@@ -6,13 +6,10 @@ from .paths import (
     get_results_root,
     get_runs_root,
 )
-from .registry import TASK_IDS, get_task_spec
 
 __all__ = [
     "get_humanoid_parkour_root",
     "get_isaaclab_root",
     "get_results_root",
     "get_runs_root",
-    "TASK_IDS",
-    "get_task_spec",
 ]
